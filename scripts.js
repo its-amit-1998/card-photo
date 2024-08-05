01e280b23962665ba2711761c3a4a5de85e1caa7
Amit Singh <amitsingh4466019@gmail.com>
2024-08-05 11:06:42 +0530

    extra:other feature like object-fit

--- scripts.js
+++ scripts.js
@@ -1,105 +1,153 @@
-window.addEventListener('load', function() {
-    const preloader = document.getElementById('preloader');
-    const cardContainer = document.getElementById('card-container');
+window.addEventListener("load", function () {
+  const preloader = document.getElementById("preloader");
+  const cardContainer = document.getElementById("card-container");
 
-    // Function to hide preloader
-    function hidePreloader() {
-        // Check if the preloader is still visible
-        if (!preloader.classList.contains('hidden')) {
-            preloader.classList.add('hidden');
-        }
-
-        // Ensure card container becomes visible
-        cardContainer.style.visibility = 'visible';
-        document.body.style.overflow = 'auto';
+  // Function to hide preloader
+  function hidePreloader() {
+    // Check if the preloader is still visible
+    if (!preloader.classList.contains("hidden")) {
+      preloader.classList.add("hidden");
     }
 
-    // Set a listener for the transition end to ensure the preloader is hidden
-    preloader.addEventListener('transitionend', function() {
-        preloader.style.display = 'none';
+    // Ensure card container becomes visible
+    cardContainer.style.visibility = "visible";
+    document.body.style.overflow = "auto";
+  }
+
+  // Set a listener for the transition end to ensure the preloader is hidden
+  preloader.addEventListener("transitionend", function () {
+    preloader.style.display = "none";
+  });
+
+  // Call the hidePreloader function when the window loads
+  window.addEventListener("load", hidePreloader);
+
+  // Fallback to ensure preloader is removed after a certain time
+  setTimeout(hidePreloader, 5000);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Camera and other functionalities...
+  document
+    .getElementById("upload-photo-button")
+    .addEventListener("click", function () {
+      startCamera();
     });
 
-    // Call the hidePreloader function when the window loads
-    window.addEventListener('load', hidePreloader);
+  function startCamera() {
+    const video = document.getElementById("camera-view");
+    const canvas = document.getElementById("camera-canvas");
+    const frame = document.getElementById("frame");
+    const takePhotoButton = document.getElementById("take-photo-button");
 
-    // Fallback to ensure preloader is removed after a certain time
-    setTimeout(hidePreloader, 5000);
-});
+    video.style.display = "block";
+    frame.style.display = "block";
+    takePhotoButton.style.display = "block";
 
-// Camera and other functionalities...
-document.getElementById('upload-photo-button').addEventListener('click', function() {
-    startCamera();
-    // document.getElementById('upload-image').click();
-});
+    navigator.mediaDevices
+      .getUserMedia({ video: { facingMode: "environment" } })
+      .then((stream) => {
+        video.srcObject = stream;
 
-// document.getElementById('upload-image').addEventListener('change', function(event) {
-//     startCamera();
-// });
+        takePhotoButton.addEventListener("click", function () {
+          canvas.width = video.videoWidth;
+          canvas.height = video.videoHeight;
+          const context = canvas.getContext("2d");
+          context.drawImage(video, 0, 0, canvas.width, canvas.height);
+          context.drawImage(frame, 0, 0, canvas.width, canvas.height);
+          const dataUrl = canvas.toDataURL("image/png");
 
-function startCamera() {
-    const video = document.getElementById('camera-view');
-    const canvas = document.getElementById('camera-canvas');
-    const frame = document.getElementById('frame');
-    const takePhotoButton = document.getElementById('take-photo-button');
-
-    video.style.display = 'block';
-    frame.style.display = 'block';
-    takePhotoButton.style.display = 'block';
-
-    navigator.mediaDevices.getUserMedia({ video: {facingMode: 'environment' } })
-        .then(stream => {
-            video.srcObject = stream;
-        })
-        .catch(error => {
-            console.error('Error accessing camera: ', error);
+          const uploadedImage = document.getElementById("uploaded-image");
+          uploadedImage.src = dataUrl;
+          uploadedImage.style.display = "block";
+          stopCamera();
         });
+      })
+      .catch((error) => {
+        console.error("Error accessing camera: ", error);
+      });
+  }
 
-        // 300 × 533 px
-    takePhotoButton.addEventListener('click', function() {
-        canvas.width = 1080; 
-        canvas.height = 1920;
-        const context = canvas.getContext('2d');
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        context.drawImage(frame, 0, 0, canvas.width, canvas.height);
-        const dataUrl = canvas.toDataURL('image/png');
-        
-        const uploadedImage = document.getElementById('uploaded-image');
-        uploadedImage.src = dataUrl;
-        uploadedImage.style.display = 'block';
-        stopCamera();
-    });
-}
-
-function stopCamera() {
-    const video = document.getElementById('camera-view');
+  function stopCamera() {
+    const video = document.getElementById("camera-view");
     const stream = video.srcObject;
     const tracks = stream.getTracks();
 
-    tracks.forEach(track => track.stop());
+    tracks.forEach((track) => track.stop());
     video.srcObject = null;
 
     // Hide camera view and frame
-    video.style.display = 'none';
-    document.getElementById('frame').style.display = 'none';
-    document.getElementById('take-photo-button').style.display = 'none';
-}
+    video.style.display = "none";
+    document.getElementById("frame").style.display = "none";
+    document.getElementById("take-photo-button").style.display = "none";
+    document.getElementById("camera-canvas").style.display = "none";
+  }
 
-document.getElementById('player-name').addEventListener('input', function() {
-    const name = this.value;
-    document.getElementById('uploaded-image').alt = name;
-});
-
-document.getElementById('player-rating').addEventListener('input', function() {
-    const rating = this.value;
-    // You can implement further logic to display the rating on the card
-});
-
-document.getElementById('save-button').addEventListener('click', function() {
-    const cardContainer = document.getElementById('card-container');
-    html2canvas(cardContainer).then(canvas => {
-        const link = document.createElement('a');
-        link.href = canvas.toDataURL('image/png');
-        link.download = 'custom-card.png';
-        link.click();
+  const playerName = document.getElementById("player-name");
+  if (playerName) {
+    playerName.addEventListener("input", function () {
+      const name = this.value;
+      document.getElementById("uploaded-image").alt = name;
     });
+  }
+
+  const playerRating = document.getElementById("player-rating");
+  if (playerRating) {
+    playerRating.addEventListener("input", function () {
+      const rating = this.value;
+      // You can implement further logic to display the rating on the card
+    });
+  }
+
+  document.getElementById("save-button").addEventListener("click", function () {
+    const uploadedImage = document.getElementById("uploaded-image");
+
+    if (!uploadedImage.src || uploadedImage.src === window.location.href) {
+      console.error("Uploaded Image not found");
+      return;
+    }
+
+    const imageContainer = document.getElementById("image-container");
+    const card = document.getElementById("card-container");
+    html2canvas(imageContainer)
+    .then((canvas) => {
+      const link = document.createElement("a");
+      link.href = canvas.toDataURL("image/png");
+      link.download = "custom-card.png";
+      link.click();
+
+      // const outputCanvas = document.createElement("canvas");
+      // const ctx = outputCanvas.getContext("2d");
+      // outputCanvas.width = 1080;
+      // outputCanvas.height = 1920;
+
+      // const inputAspect = canvas.width / canvas.height;
+      // const outputAspect = outputCanvas.width / outputCanvas.height;
+
+      // let drawWidth, drawHeight;
+      // let offsetX = 0, offsetY = 0;
+
+      // if (inputAspect > outputAspect) {
+      //     // Input image is wider than output aspect ratio
+      //     drawHeight = outputCanvas.height;
+      //     drawWidth = drawHeight * inputAspect;
+      //     offsetX = (outputCanvas.width - drawWidth) / 2;
+      // } else {
+      //     // Input image is taller than output aspect ratio
+      //     drawWidth = outputCanvas.width;
+      //     drawHeight = drawWidth / inputAspect;
+      //     offsetY = (outputCanvas.height - drawHeight) / 2;
+      // }
+
+      // ctx.drawImage(canvas, offsetX, offsetY, drawWidth, drawHeight);
+
+      // const link = document.createElement("a");
+      // link.href = outputCanvas.toDataURL("image/png");
+      // link.download = "custom-card.png";
+      // link.click();
+    })
+    .catch((error) => {
+      console.error("Error converting image to PNG:", error);
+    });
+  });
 });
